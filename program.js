a142982dc93fb97d8e365c070b20fd9c0660d727
Elizabeth Hale <lizzhale@mac.com>
2015-03-28 14:11:40 -0700

    My First I/O

--- program.js
+++ program.js
@@ -1,5 +1,4 @@
-var sum = 0
-for (var i = 2; i < process.argv.length; i++) {
-  sum += +process.argv[i];
-}
-console.log(sum);
+var fs = require('fs');
+var file = fs.readFileSync(process.argv[2]).toString();
+var fileArr = file.split('\n');
+console.log(fileArr.length - 1);
