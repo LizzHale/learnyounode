f2cb0a7165656162f87b2e43ebcf0fa4ef0fa687
Elizabeth Hale <lizzhale@mac.com>
2015-03-28 14:41:46 -0700

    Async I/O

--- program.js
+++ program.js
@@ -1,4 +1,10 @@
 var fs = require('fs');
-var file = fs.readFileSync(process.argv[2]).toString();
-var fileNum = file.split('\n').length - 1;
-console.log(fileNum);
+
+var callback = function(err, data) {
+    if (err) return console.log("Error");
+    data = data.toString();
+    var fileNum = data.split('\n').length -1;
+    console.log(fileNum);
+};
+fs.readFile(process.argv[2], callback);
+
