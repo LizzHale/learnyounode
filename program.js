34a795a44659a7817650ebd8cffd1aaa2af26f6f
Elizabeth Hale <lizzhale@mac.com>
2015-03-28 14:43:22 -0700

    Refined to use utf8 parameter instead of toString function)

--- program.js
+++ program.js
@@ -2,9 +2,9 @@ var fs = require('fs');
 
 var callback = function(err, data) {
     if (err) return console.log("Error");
-    data = data.toString();
     var fileNum = data.split('\n').length -1;
     console.log(fileNum);
 };
-fs.readFile(process.argv[2], callback);
+
+fs.readFile(process.argv[2], 'utf8', callback);
 
