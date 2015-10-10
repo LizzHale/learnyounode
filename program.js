938fe93e4b33ef6b79037f064601af813e9a65be
Elizabeth Hale <lizzhale@mac.com>
2015-10-10 11:49:01 -0700

    HTTP JSON API Server complete

--- program.js
+++ program.js
@@ -1,19 +1,48 @@
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
-var map = require('through2-map');
 
 // Port number is supplied as the first commandline argument
 var port = process.argv[2];
 
-
-
-var server = http.createServer(function(request, response) {
-    if (request.method != 'POST') {
-        return response.end('send me a POST\n');
+function parsetime(time) {
+    return {
+        "hour": time.getHours(),
+        "minute": time.getMinutes(),
+        "second": time.getSeconds()
     }
-    request.pipe(map(function(chunk) {
-        return chunk.toString().toUpperCase();
-    })).pipe(response);
+}
+
+function unixtime(time) {
+    return {
+        "unixtime": time.getTime()
+    }
+}
+
+// two routes:
+// One route /api/parsetime  with query string ?iso=<iso-format time>
+// Send a json response containing hour minute and second
+// Second route /api/unixtime with query string ?iso=<iso-format time>
+// Send a json response containing unix epoch time
+var server = http.createServer(function(request, response) {
+    var urlParsed = url.parse(request.url, true);
+    var time = new Date(urlParsed.query.iso);
+
+    if (urlParsed.pathname === '/api/parsetime') {
+        result = parsetime(time);
+    }
+    else if (urlParsed.pathname === '/api/unixtime') {
+        result = unixtime(time);
+    }
+
+    if (result) {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(result));
+    }
+    else {
+        response.writeHead(404);
+        response.end();
+    }
 });
 
 server.listen(port);
